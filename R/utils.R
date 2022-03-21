ff57f0e82c3beb7df538ab195fce1a56f1f929fa
ryanchrist <ryan.christ7@gmail.com>
2022-03-21 10:10:59 -0500

    bolster numerical error checking for traces vs. eigenvalues for input to QForm

--- R/utils.R
+++ R/utils.R
@@ -213,8 +213,9 @@ CalcBounds2 <- function(traces, e = NULL, tau = 1, delta = 0, only.point.est = F
     })
   }
 
-  if(length(e$values)==length(traces$diag) |
-     (traces$hsnorm2 - sum(e$values^2)) <= 0 ){
+  if(length(e$values)==length(traces$diag) ||
+     (1 - sum(e$values^2)/traces$hsnorm2) <= sqrt(.Machine$double.eps) ||
+     traces$hsnorm2 - sum(e$values^2) <= .Machine$double.eps){
     # bounds are not needed because either we have all of the eigenvalues or the eigenvalues that
     # are in the remainder are non-zero due to numerical imprecision.
     gauss.tcdf <- QForm::QFGauss(tau * e$values, parallel.sapply = parallel.sapply)
